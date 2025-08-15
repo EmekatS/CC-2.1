d6f8306e600b77688da4a5b72dfccb65582f7182
[Veritas 10167] <>
2025-08-15 18:51:09 +0100

    initial

---
+++ script.js
@@ -0,0 +1,6 @@
+let sign_in_btn = document.querySelector(".sign-in");
+let sign_up_btn = document.querySelector(".sign-up");
+
+sign_in_btn.onclick = () => {
+    window.location.href = "sign-in.html";
+}
