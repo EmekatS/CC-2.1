da8923970f452d3758565fb4714823a42d9c4e49
[Veritas 10167] <>
2025-08-16 16:53:08 +0100

    sign-up page work

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let sign_in_btn = document.querySelector(".sign-in");
 let sign_up_btn = document.querySelector(".sign-up");
 
-sign_in_btn.onclick = () => {
+sign_in_btn.addEventListener("click",() => {
     window.location.href = "sign-in.html";
-}
+})
+sign_up_btn.addEventListener("click",() => {
+    window.location.href = "sign-up.html";
+})
