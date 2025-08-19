c391e11a7ef04a846bc76adace693a92067969b3
[Veritas 10167] <>
2025-08-19 17:23:23 +0100

    sign in page

--- script.js
+++ script.js
@@ -1,16 +1,37 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const signInBtn = document.querySelector('.sign-in');
-  const signUpBtn = document.querySelector('.sign-up');
+// document.addEventListener('DOMContentLoaded', () => {
+//   const signInBtn = document.querySelector('.sign-in');
+//   const signUpBtn = document.querySelector('.sign-up');
+//   const finish = document.querySelector('.finish')
 
-  if (signInBtn) {
-    signInBtn.addEventListener('click', () => {
-      window.location.href = 'sign-in.html';
-    });
-  }
+//   if (signInBtn) {
+//     signInBtn.addEventListener('click', () => {
+//       window.location.href = 'sign-in.html';
+//     });
+//   }
 
-  if (signUpBtn) {
-    signUpBtn.addEventListener('click', () => {
-      window.location.href = 'sign-up.html';
-    });
-  }
-});
+//   if (signUpBtn) {
+//     signUpBtn.addEventListener('click', () => {
+//       window.location.href = 'sign-up.html';
+//     });
+//   }
+
+//   if (finish) {
+//     signUpBtn.addEventListener('click', () => {
+//       window.location.href = 'intrests.html';
+//     });
+//   }
+// });
+
+const sign_in_btn = document.querySelector('.sign-in');
+const sign_up_btn = document.querySelector('.sign-up');
+const finish = document.querySelector('.finish')
+
+sign_in_btn.addEventListener("click",() => {
+    window.location.href = "sign-in.html"
+})
+sign_up_btn.addEventListener("click",() => {
+    window.location.href = "sign-up.html"
+})
+finish.addEventListener("click",() => {
+    window.location.href = "intrests.html"
+})
